7c902666675fff98ecd7661aebc4da6b9b0af32a
aguldstrand <d>
2013-08-30 22:10:20 +0200

    command stuffs

--- uifx/Command.ts
+++ uifx/Command.ts
@@ -1,19 +1,54 @@
 ///<Reference path="ILockModel.ts" />
 module uifx {
     export class Command {
-        constructor(private lockLevel: number, private lockModel: ILockModel) {
+        constructor(private timeout: number, private lockLevel: number, private lockModel: ILockModel) {
         }
 
-        public execute(input: any) {
+
+        public execute(input) {
+            this.lock();
+            var canceled = false;
+
+            if (this.timeout) {
+                var timerHandle = setTimeout(() => {
+                    canceled = true;
+                    this.unlock();
+                }, this.timeout);
+            }
+
+            this.executeImpl(input, (err, data) => {
+                if (!canceled) {
+                    clearTimeout(timerHandle);
+                    this.complete(err, data);
+                }
+            });
+        }
+
+        public complete(err, data) {
+            this.completeImpl(err, data);
+            this.unlock();
+        }
+
+
+        private lock() {
             if (this.lockModel) {
                 this.lockModel.lock(this.lockLevel);
             }
         }
 
-        public complete(error: any, result: any) {
+        private unlock() {
             if (this.lockModel) {
                 this.lockModel.unlock(this.lockLevel);
             }
+        }
+
+
+        public executeImpl(input, callback: (err, data) => void) {
+            throw "Not implemented";
+        }
+
+        public completeImpl(err, data) {
+            throw "Not implemented";
         }
     }
 }
